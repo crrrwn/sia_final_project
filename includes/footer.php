0b5e0dbbdebb414bcfd4c89d9391bbf2c76721b0
aniemarie21 <aniemariearellano5@gmail.com>
2024-12-02 21:35:23 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,30 @@
-<footer class="text-center text-white py-2" style="background-color: #006400;">
+<footer class="text-center py-2" style="background-color: #fff; color: #000;">
     <div class="container">
         <p class="mb-0" style="font-size: 1rem;">© 2023 Blog System | All Rights Reserved</p>
         <div class="mt-2">
-            <a href="https://facebook.com" class="text-white mx-2" target="_blank" aria-label="Facebook">
+            <a href="https://facebook.com" class="text-black mx-2" target="_blank" aria-label="Facebook" style="color: #000;">
                 <i class="fab fa-facebook"></i>
             </a>
-            <a href="https://twitter.com" class="text-white mx-2" target="_blank" aria-label="Twitter">
+            <a href="https://twitter.com" class="text-black mx-2" target="_blank" aria-label="Twitter" style="color: #000;">
                 <i class="fab fa-twitter"></i>
             </a>
-            <a href="https://instagram.com" class="text-white mx-2" target="_blank" aria-label="Instagram">
+            <a href="https://instagram.com" class="text-black mx-2" target="_blank" aria-label="Instagram" style="color: #000;">
                 <i class="fab fa-instagram"></i>
             </a>
-            <a href="https://linkedin.com" class="text-white mx-2" target="_blank" aria-label="LinkedIn">
+            <a href="https://linkedin.com" class="text-black mx-2" target="_blank" aria-label="LinkedIn" style="color: #000;">
                 <i class="fab fa-linkedin"></i>
             </a>
-            <a href="https://youtube.com" class="text-white mx-2" target="_blank" aria-label="YouTube">
+            <a href="https://youtube.com" class="text-black mx-2" target="_blank" aria-label="YouTube" style="color: #000;">
                 <i class="fab fa-youtube"></i>
             </a>
-            <a href="https://pinterest.com" class="text-white mx-2" target="_blank" aria-label="Pinterest">
+            <a href="https://pinterest.com" class="text-black mx-2" target="_blank" aria-label="Pinterest" style="color: #000;">
                 <i class="fab fa-pinterest"></i>
             </a>
         </div>
     </div>
 </footer>
 
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core
+
